e738e99ccbc19735e1e11891b07e23d2ceed2254
dsofttech <dynamicsofttechnology@gmail.com>
2023-04-05 13:01:41 +0100

    removed hardcoded sensitive information

--- nibssclient/build/intermediates/cxx/Release/171w5f5d/obj/local/arm64-v8a/objs/module-credentials/module-credentials.o.d
+++
@@ -1,2 +0,0 @@
-/Users/adebayooloyede/Desktop/SAdebayoAndroid/NibssClientLibrary/nibssclient/build/intermediates/cxx/Release/171w5f5d/obj/local/arm64-v8a/objs/module-credentials/module-credentials.o: \
-  /Users/adebayooloyede/Desktop/SAdebayoAndroid/NibssClientLibrary/nibssclient/src/main/jni/module-credentials.c
--- nibssclient/build/intermediates/cxx/Release/171w5f5d/obj/local/armeabi-v7a/objs/module-credentials/module-credentials.o.d
+++
@@ -1,2 +0,0 @@
-/Users/adebayooloyede/Desktop/SAdebayoAndroid/NibssClientLibrary/nibssclient/build/intermediates/cxx/Release/171w5f5d/obj/local/armeabi-v7a/objs/module-credentials/module-credentials.o: \
-  /Users/adebayooloyede/Desktop/SAdebayoAndroid/NibssClientLibrary/nibssclient/src/main/jni/module-credentials.c
